ef48ece71fe7470aefb0e51b2540c512e8769857
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-11-16 00:57:30 +0100

    rename SenatorInitializer to PacketBroker. it creates a queue for each service and pass generated packet to each service (thread)

--- src/main/java/artronics/senator/core/Main.java
+++ src/main/java/artronics/senator/core/Main.java
@@ -1,29 +1,21 @@
 package artronics.senator.core;
 
-import artronics.chaparMini.Chapar;
 import artronics.chaparMini.exceptions.ChaparConnectionException;
-import artronics.senator.core.log.Log;
-
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main
 {
     public static void main(String args[]){
+        PacketBroker initializer = new PacketBroker();
 
-        LinkedList<List<Integer>> receivedBuffer = new LinkedList<>();
-        LinkedList<List<Integer>> transmitBuffer = new LinkedList<>();
-
-        Log.MAIN.debug("sen");
-        Chapar chapar = new Chapar(receivedBuffer, transmitBuffer);
         try {
-            chapar.connect();
+            initializer.start();
         }catch (ChaparConnectionException e) {
             e.printStackTrace();
-            Log.FILE.error(e.getMessage());
         }
 
-        Thread chaparThr = new Thread(chapar,"Chapar");
-        chapar.run();
+        Thread init = new Thread(initializer);
+        init.start();
+
+
     }
 }
