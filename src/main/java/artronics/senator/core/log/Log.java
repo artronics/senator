ac470e9a4fdfe15341dd592b85cc82cd7d280053
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-11-17 05:12:28 +0100

    complete mapUpdater and add test. change graphDelegator accordigly for adding some graph helper methods.

--- src/main/java/artronics/senator/core/log/Log.java
+++ src/main/java/artronics/senator/core/log/Log.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 
 public interface Log
 {
-    Logger MAIN = Logger.getLogger("main");
+    Logger SEN = Logger.getLogger("senator");
     Logger FILE = Logger.getLogger("file");
 
 }
