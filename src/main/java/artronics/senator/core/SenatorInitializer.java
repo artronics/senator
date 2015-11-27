a0afb64e8fb0e77c9ec568bbccce493e635b7666
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-11-27 21:41:01 +0100

    create test for senatorInitializer. add init method which retrive latest config and creates a new one if dosen't exist

--- src/main/java/artronics/senator/core/SenatorInitializer.java
+++ src/main/java/artronics/senator/core/SenatorInitializer.java
@@ -1,14 +1,19 @@
 package artronics.senator.core;
 
 import artronics.gsdwn.controller.Controller;
+import artronics.gsdwn.model.ControllerConfig;
 import artronics.senator.services.ControllerConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityNotFoundException;
+
 @Component
 public class SenatorInitializer
 {
-    private Controller controller;
+    private final Controller controller;
+
+    private ControllerConfig controllerConfig;
 
     @Autowired
     private ControllerConfigService controllerService;
@@ -21,6 +26,16 @@ public class SenatorInitializer
 
     public void init()
     {
+        //get latest config. if there is no config yet create one
+        try {
+            controllerConfig = controllerService.getLatest();
+
+        }catch (EntityNotFoundException e) {
+            controllerConfig = new ControllerConfig();
+            controllerConfig.setIp("192.168.1.1");
+
+            controllerService.create(controllerConfig);
+        }
 
 
     }
---
+++ src/test/java/artronics/senator/core/SenatorInitializerTest.java
@@ -0,0 +1,51 @@
+package artronics.senator.core;
+
+import artronics.gsdwn.model.ControllerConfig;
+import artronics.senator.services.ControllerConfigService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:senator-beans.xml")
+public class SenatorInitializerTest
+{
+    @Autowired
+    SenatorInitializer initializer;
+
+    @Autowired
+    ControllerConfigService configService;
+
+    ControllerConfig cnf;
+
+    @Before
+    @Transactional
+    @Rollback(value = false)
+    public void setUp() throws Exception
+    {
+
+
+    }
+
+    @Test
+    @Transactional
+    public void test_init_it_should_create_default_config_if_there_is_no_config()
+    {
+        initializer.init();
+
+        cnf = configService.getLatest();
+
+        assertNotNull(cnf);
+        assertThat(cnf.getIp(), equalTo("192.168.1.1"));
+    }
+
+}
