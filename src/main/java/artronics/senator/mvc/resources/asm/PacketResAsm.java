8b13d4f411c5f937849775ad4374467e440eddce
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-01 02:43:13 +0100

    add packet res

---
+++ src/main/java/artronics/senator/mvc/resources/asm/PacketResAsm.java
@@ -0,0 +1,29 @@
+package artronics.senator.mvc.resources.asm;
+
+import artronics.gsdwn.packet.SdwnBasePacket;
+import artronics.senator.mvc.resources.PacketRes;
+import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
+
+public class PacketResAsm extends ResourceAssemblerSupport<SdwnBasePacket, PacketRes>
+{
+    public PacketResAsm()
+    {
+        super(SdwnBasePacket.class, PacketRes.class);
+    }
+
+    @Override
+    public PacketRes toResource(SdwnBasePacket packet)
+    {
+        PacketRes res = new PacketRes();
+
+        res.setRid(packet.getId());
+        res.setNetId(packet.getNetId());
+        res.setSrcShortAdd(packet.getSrcShortAddress());
+        res.setDstShortAdd(packet.getDstShortAddress());
+        res.setTtl(packet.getTtl());
+        res.setNextHop(packet.getNextHop());
+        res.setContent(packet.getContent());
+
+        return res;
+    }
+}
