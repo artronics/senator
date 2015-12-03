da96d2a42035f0ae374cb8e37e69fed5b0b04bd9
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-03 18:46:09 +0100

    update

--- src/main/java/artronics/senator/mvc/controllers/PacketController.java
+++ src/main/java/artronics/senator/mvc/controllers/PacketController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,6 +36,7 @@ public class PacketController
         return new ResponseEntity<PacketListRes>(packetListRes, HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://localhost:9000")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<PacketListRes> getAllPackets()
     {
