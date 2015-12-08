98da537f5f084e2c201f32032f6fbbad7e7cbd71
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-08 21:11:28 +0100

    add a config class for senator

---
+++ src/main/java/artronics/senator/core/SenatorConfig.java
@@ -0,0 +1,15 @@
+package artronics.senator.core;
+
+public class SenatorConfig
+{
+    private String controllerIp;
+
+    public SenatorConfig()
+    {
+    }
+
+    public SenatorConfig(String controllerIp)
+    {
+        this.controllerIp = controllerIp;
+    }
+}
